368431e757fcc5a78215141b172ead843470bcfe
Jongheyok <166394117+Jongheyok@users.noreply.github.com>
2024-05-14 02:19:52 +0900

    Update kakaoMpas.js

--- kakaoMpas.js
+++ kakaoMpas.js
@@ -1,7 +1,7 @@
 function kakaoMap(){
   var container = document.getElementById('map');
 var options = {
-  center: new kakao.maps.LatLng(33.450701, 126.570667),
+  center: new kakao.maps.LatLng(37.5846465, 126.9251874),
   lvel: 3
 };
 
