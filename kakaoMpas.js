fde37bf78eb017f75324872fa87fdb843f71f7cd
Jongheyok <whdgur3960@naver.com>
2024-05-14 19:53:02 +0900

    마커추가

--- kakaoMpas.js
+++ kakaoMpas.js
@@ -4,10 +4,13 @@ var options = {
   center: new kakao.maps.LatLng(37.5846465, 126.9251874),
   lvel: 3
 };
-
+var markerPosition  = new kakao.maps.LatLng(37.5846465, 126.9251874); 
+var marker = new kakao.maps.Marker({
+  position: markerPosition
+});
 var map = new kakao.maps.Map(container, options);
 };
-
+marker.setMap(map);
 $(function(){
   kakaoMap();
 })
