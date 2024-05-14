8529dfbd3a01eb769af96ea9ff9ba5fd4dc89389
Jongheyok <whdgur3960@naver.com>
2024-05-14 20:31:28 +0900

    js수정

--- kakaoMpas.js
+++ kakaoMpas.js
@@ -1,14 +1,14 @@
 function kakaoMap(){
-  var container = document.getElementById('map');
-var options = {
+  var mapcontainer = document.getElementById('map');
+var mapoptions = {
   center: new kakao.maps.LatLng(37.5846465, 126.9251874),
   level: 3
 };
 var markerPosition  = new kakao.maps.LatLng(37.5846465, 126.9251874); 
+var map = new kakao.maps.Map(mapContainer, mapOption);
 var marker = new kakao.maps.Marker({
   position: markerPosition
 });
-var map = new kakao.maps.Map(container, options);
 marker.setMap(map);
 };
 
