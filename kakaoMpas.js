4f3705f8c47fbc282411e8c545da41e0cac90608
Jongheyok <whdgur3960@naver.com>
2024-05-14 20:17:26 +0900

    오타수정

--- kakaoMpas.js
+++ kakaoMpas.js
@@ -2,7 +2,7 @@ function kakaoMap(){
   var container = document.getElementById('map');
 var options = {
   center: new kakao.maps.LatLng(37.5846465, 126.9251874),
-  lvel: 3
+  level: 3
 };
 var markerPosition  = new kakao.maps.LatLng(37.5846465, 126.9251874); 
 var marker = new kakao.maps.Marker({
